8cddbc2d7b0c89d3aa053fea3759f93da7fd070d
APSN4 <tema.sergeevich2012@yandex.ru>
2025-05-24 09:54:48 -0700

    feature: links_dataset.py

--- tutu-scraping/links_dataset.py
+++ tutu-scraping/links_dataset.py
@@ -12,12 +12,16 @@ r = requests.get('https://www.tutu.ru/poezda/vkz/')
 soup = BeautifulSoup(r.content, 'html.parser')
 
 items = soup.find_all('a', class_='b-train--vokzal_list--links__link')
-links = []
+objects = []
 data = {}
 for item in items:
-    links.append("https://www.tutu.ru" + item.get('href'))
+    data_local = {
+        "title": item.text,
+        "link": "https://www.tutu.ru" + item.get('href')
+    }
+    objects.append(data_local)
 
-data["links"] = links
+data["links"] = objects
 
 with open("data_links.json", "w", encoding="utf-8") as outfile:
     json.dump(data, outfile, ensure_ascii=False)
