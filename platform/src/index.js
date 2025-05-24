da73abc75746510008f9eea857b405cd2a8e814f
APSN4 <tema.sergeevich2012@yandex.ru>
2025-05-24 03:23:10 -0700

    feature: points on the map

--- platform/src/index.js
+++ platform/src/index.js
@@ -8,3 +8,17 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
         '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+
+fetch("storage/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+        data.train_stations.forEach((station) => {
+            if (station.position == null) {
+                return
+            }
+            const marker = L.marker([station.position[0], station.position[1]])
+                .addTo(map)
+                .bindPopup(`<b>${station.title}</b><br>${station.address}`);
+        });
+    })
+    .catch((err) => console.error("Ошибка загрузки данных:", err));
